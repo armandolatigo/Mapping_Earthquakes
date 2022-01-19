da28b41fdd7d9883392bfb4c5e1f1e0bbae6debf
armandolatigo <92451164+armandolatigo@users.noreply.github.com>
2022-01-19 17:07:07 -0600

    logic.js modifications with circleMarkers

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -13,8 +13,31 @@ let map = L.map("mapid").setView([40.7, -94.5],4);
 //     zoom: 4  
 // });
 
+// Add a marker to the map for Los Angeles, California
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Austin, TX (for fun)
+let marker2 = L.marker([30.2672, -97.7431]).addTo(map);
+
+//Add circle marker to map for LA, Cali.
+//  L.circle([34.0522, -118.2437], {
+//      color: 'blue',
+//      fillColor: '#FFFF00',
+//      fillOPacity: 0.75,
+//      radius:7000
+//  }).addTo(map);
+
+// circleMarker() method
+
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+
 // Create the tile layer that will be the background of the map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
